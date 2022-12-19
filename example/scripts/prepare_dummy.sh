9d1b187ac45866ba222dcf03adf9a68b54eb50c9
haukurb <haukzi@gmail.com>
2022-12-19 23:16:09 +0000

    Parallel staircase with fixed chunk size now runs

--- example/scripts/prepare_dummy.sh
+++ example/scripts/prepare_dummy.sh
@@ -12,14 +12,14 @@ done
 > data/dummy.txt
 > data/dummy_tgt.txt
 for _ in {1..100} ; do
-    # NOTE: these are of odd length, which with bos token becomes even length
-    #       which is divisible by 2 (the shorten_factor, assertion in resampling_transformer_layer)
     echo "0 1 2 3 4" >> data/dummy.txt
     echo "5 4 3" >> data/dummy.txt
     echo "9 8 7 5 4" >> data/dummy.txt
     echo "5 4 3 2 1 1 8" >> data/dummy.txt
     echo "5 5 5 5 5 2 8 2 8" >> data/dummy.txt
-    echo "5 5 5 5 5 2 8 2 8" >> data/dummy.txt
+    echo "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99" >> data/dummy.txt
+    echo "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49" >> data/dummy.txt
+    echo "10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58" >> data/dummy.txt
 
     echo "9 3 4" >> data/dummy_tgt.txt
     echo "9" >> data/dummy_tgt.txt
--- fairseq_user_dir/model.py
+++ fairseq_user_dir/model.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Any, Dict, List, Optional
+from collections import deque
+from itertools import cycle, islice
+
+from torch import Tensor
+import torch
 
 from icecream import ic
 
@@ -9,49 +14,15 @@ from fairseq.models.transformer import TransformerConfig
 from fairseq.utils import safe_getattr
 from fairseq.models.transformer_lm import (
     TransformerLanguageModel,
-    TransformerLanguageModelConfig,
 )
-from fairseq.modules import AdaptiveInput, CharacterTokenEmbedder
 from fairseq.models.transformer.transformer_decoder import (
     TransformerDecoderBase,
 )
 from fairseq.modules.transformer_layer import TransformerDecoderLayerBase
 from fairseq.models import (
-    # FairseqLanguageModel,
     register_model,
     register_model_architecture,
 )
-
-DEFAULT_MAX_TARGET_POSITIONS = 1024
-
-# TransformerLanguageModel -> fairseq/models/transformer_lm.py
-# TransformerDecoder -> fairseq/models/transformer/transformer_decoder.py
-# TransformerDecoderBase -> fairseq/models/transformer/transformer_decoder.py
-# TransformerDecoderLayerBase -> fairseq/modules/transformer_layer.py
-# FairseqLanguageModel -> fairseq/models/fairseq_model.py
-# fairseq/model_parallel/models/pipeline_parallel_transformer/model.py
-
-"""
-TransformerLanguageModel registers the arch "transformer_lm"
-TransformerLanguageModel inherits from FairseqLanguageModel
-TransformerLanguageModel builds TransformerDecoder
-    TransformerDecoder inherits from TransformerDecoderBase which inherits from FairseqIncrementalDecoder
-        # TransformerDecoder is just a thin wrapper
-        TransformerDecoderBase
-            # calls build_decoder_layer and delegates to transformer_layer.TransformerDecoderLayerBase
-            # has the actual decoder forward pass
-            TransformerDecoderLayerBase
-                # builds the multiheadattention
-
-
-to get staircase we just need to subclass this and register it:
-- TransformerLanguageModel
-
-to get alibi attention we need to do more surgical things:
-- we need to subclass TransformerDecoderBase and overwrite its build_decoder_layer method
-- we need to subclass TransformerDecoderLayerBase and overwrite its build_self_attention method 
-- we need to subclass TransformerLanguageModel and overwrite its build_model function to use the new TransformerDecoderLayerBase
-"""
 
 
 @dataclass
@@ -62,22 +33,41 @@ class StaircaseTransformerConfig(TransformerConfig):
     use_alibi: bool = field(default=False)
 
     def __post_init__(self):
-        self.decoder.layers = self.num_bottom_layers + self.num_staircase_layers + self.num_top_layers
+        self.decoder.layers = (
+            self.num_bottom_layers + self.num_staircase_layers + self.num_top_layers
+        )
 
 
-# @dataclass
-# class StaircaseTransformerLanguageModelConfig(TransformerLanguageModelConfig):
-#     num_base_layers: int = field(default=0)
-#     num_top_layers: int = field(default=1)
-#     num_staircase_layers: int = field(default=4)
-#     use_alibi: bool = field(default=False)
-#     staircase: StaircaseTransformerConfig = field(default_factory=StaircaseTransformerConfig)
-#     # pass
-
-
-# @register_model("staircase_lm", dataclass=StaircaseTransformerLanguageModelConfig)
 @register_model("staircase_lm", dataclass=StaircaseTransformerConfig)
 class StaircaseTransformerDecoderModel(TransformerLanguageModel):
+    """ structure of arch=transformer_lm:
+    TransformerLanguageModel registers the arch "transformer_lm"
+    TransformerLanguageModel inherits from FairseqLanguageModel
+    TransformerLanguageModel builds TransformerDecoder
+        TransformerDecoder inherits from TransformerDecoderBase which inherits from FairseqIncrementalDecoder
+            # TransformerDecoder is just a thin wrapper
+            TransformerDecoderBase
+                # calls build_decoder_layer and delegates to transformer_layer.TransformerDecoderLayerBase
+                # has the actual decoder forward pass
+                TransformerDecoderLayerBase
+                    # builds the multiheadattention
+
+    to get staircase we just need to subclass the following and register it:
+    - TransformerLanguageModel
+
+    to get alibi attention we need to do more surgical things:
+    - we need to subclass TransformerDecoderBase and overwrite its build_decoder_layer method
+    - we need to subclass TransformerDecoderLayerBase and overwrite its build_self_attention method 
+    - we need to subclass TransformerLanguageModel and overwrite its build_model function to use the new TransformerDecoderLayerBase
+
+    where the modules are located in:
+    - TransformerLanguageModel -> fairseq/models/transformer_lm.py
+    - TransformerDecoder -> fairseq/models/transformer/transformer_decoder.py
+    - TransformerDecoderBase -> fairseq/models/transformer/transformer_decoder.py
+    - TransformerDecoderLayerBase -> fairseq/modules/transformer_layer.py
+    - FairseqLanguageModel -> fairseq/models/fairseq_model.py
+    - fairseq/model_parallel/models/pipeline_parallel_transformer/model.py
+    """
     def __init__(self, decoder):
         super().__init__(decoder)
 
@@ -103,7 +93,7 @@ class StaircaseTransformerDecoderModel(TransformerLanguageModel):
 class StaircaseTransformerDecoder(TransformerDecoderBase):
     def __init__(
         self,
-        cfg: TransformerConfig,
+        cfg: StaircaseTransformerConfig,
         dictionary,
         embed_tokens,
         no_encoder_attn=False,
@@ -116,10 +106,26 @@ class StaircaseTransformerDecoder(TransformerDecoderBase):
             no_encoder_attn=no_encoder_attn,
             output_projection=output_projection,
         )
+        assert self.cfg.decoder.layerdrop == 0, "Not implemented yet"
+        bottom_range = list(range(cfg.num_bottom_layers))
+        staircase_range = list(
+            range(len(bottom_range), len(bottom_range) + cfg.num_staircase_layers)
+        )
+        top_range = range(
+            len(bottom_range + staircase_range),
+            len(bottom_range + staircase_range) + cfg.num_top_layers,
+        )
+        self.bottom_layers, self.staircase_layers, self.top_layers = [], [], []
+        for i in bottom_range:
+            self.bottom_layers.append(self.layers[i])
+        for i in staircase_range:
+            self.staircase_layers.append(self.layers[i])
+        for i in top_range:
+            self.top_layers.append(self.layers[i])
 
     def build_decoder_layer(self, cfg, no_encoder_attn=False):
         if cfg.use_alibi:
-            assert False
+            assert False, "Not implemented yet"
             layer = TransformerAlibiDecoderLayerBase(cfg, no_encoder_attn)
         else:
             layer = TransformerDecoderLayerBase(cfg, no_encoder_attn)
@@ -133,19 +139,198 @@ class StaircaseTransformerDecoder(TransformerDecoderBase):
         layer = fsdp_wrap(layer, min_num_params=min_params_to_wrap)
         return layer
 
+    def output_layer(self, features):
+        """Project features to the vocabulary size."""
+        if self.adaptive_softmax is None:
+            # project back to size of vocabulary
+            return self.output_projection(features)  # type: ignore
+        else:
+            return features
+
     """
     self.forward delegates to this function
+
+    A scriptable subclass of this class has an extract_features method and calls
+    super().extract_features, but super() is not supported in torchscript. A copy of
+    this function is made to be used in the subclass instead.
     """
 
-    # def extract_features_scriptable(
-    #     self,
-    #     prev_output_tokens,
-    #     encoder_out: Optional[Dict[str, List[Tensor]]],
-    #     incremental_state: Optional[Dict[str, Dict[str, Optional[Tensor]]]] = None,
-    #     full_context_alignment: bool = False,
-    #     alignment_layer: Optional[int] = None,
-    #     alignment_heads: Optional[int] = None,
-    # ):
+    def extract_features_scriptable(
+        self,
+        prev_output_tokens,
+        encoder_out: Optional[Dict[str, List[Tensor]]],
+        incremental_state: Optional[Dict[str, Dict[str, Optional[Tensor]]]] = None,
+        full_context_alignment: bool = False,
+        alignment_layer: Optional[int] = None,
+        alignment_heads: Optional[int] = None,
+    ):
+        """
+        Similar to *forward* but only return features.
+
+        Includes several features from "Jointly Learning to Align and
+        Translate with Transformer Models" (Garg et al., EMNLP 2019).
+
+        Args:
+            full_context_alignment (bool, optional): don't apply
+                auto-regressive mask to self-attention (default: False).
+            alignment_layer (int, optional): return mean alignment over
+                heads at this layer (default: last layer).
+            alignment_heads (int, optional): only average alignment over
+                this many heads (default: all heads).
+
+        Returns:
+            tuple:
+                - the decoder's features of shape `(batch, tgt_len, embed_dim)`
+                - a dictionary with any model-specific outputs
+        """
+        bsz, slen = prev_output_tokens.size()
+        if alignment_layer is None:
+            alignment_layer = self.num_layers - 1
+
+        enc: Optional[Tensor] = None
+        encoder_padding_mask: Optional[Tensor] = None
+        if encoder_out is not None and len(encoder_out["encoder_out"]) > 0:
+            enc = encoder_out["encoder_out"][0]
+        if encoder_out is not None and len(encoder_out["encoder_padding_mask"]) > 0:
+            encoder_padding_mask = encoder_out["encoder_padding_mask"][0]
+
+        # embed positions
+        positions = None
+        if self.embed_positions is not None:
+            positions = self.embed_positions(
+                prev_output_tokens, incremental_state=incremental_state
+            )
+
+        if incremental_state is not None:
+            prev_output_tokens = prev_output_tokens[:, -1:]
+            if positions is not None:
+                positions = positions[:, -1:]
+
+        # Prevent torchscript exporting issue for dynamic quant embedding
+        prev_output_tokens = prev_output_tokens.contiguous()
+        # embed tokens and positions
+        x = self.embed_scale * self.embed_tokens(prev_output_tokens)
+
+        if self.quant_noise is not None:
+            x = self.quant_noise(x)
+
+        if self.project_in_dim is not None:
+            x = self.project_in_dim(x)
+
+        if positions is not None:
+            x += positions
+
+        if self.layernorm_embedding is not None:
+            x = self.layernorm_embedding(x)
+
+        x = self.dropout_module(x)
+
+        # B x T x C -> T x B x C
+        x = x.transpose(0, 1)
+
+        self_attn_padding_mask: Optional[Tensor] = None
+        if self.cross_self_attention or prev_output_tokens.eq(self.padding_idx).any():
+            self_attn_padding_mask = prev_output_tokens.eq(self.padding_idx)
+
+        # decoder layers
+        attn: Optional[Tensor] = None
+        inner_states: List[Optional[Tensor]] = [x]
+        # for idx, layer in enumerate(self.layers):
+        for idx, layer in enumerate(self.bottom_layers):
+            if incremental_state is None and not full_context_alignment:
+                self_attn_mask = self.buffered_future_mask(x)
+            else:
+                self_attn_mask = None
+            x, layer_attn, _ = layer(
+                x,
+                None,  # enc_out
+                None,  # encoder_padding_mask
+                incremental_state,
+                self_attn_mask=self_attn_mask,
+                self_attn_padding_mask=self_attn_padding_mask,
+                need_attn=False,
+                need_head_weights=False,
+            )
+            inner_states.append(x)
+
+        _, _, edim = x.shape
+
+        _x_before_staircase = x
+        # used a fixed chunk size as scaffolding for now
+        fixed_chunk_len = 4
+        chunks = x.split(
+            fixed_chunk_len, dim=0
+        )  # x is of shape [SeqLen, Batch, NumHidden]
+
+        chunk_queue = deque(chunks)
+        num_forwards = len(self.staircase_layers) + len(chunks)
+        staircase_start = torch.randint(0, len(self.staircase_layers), size=[1])[0]
+        rolled_staircase = (
+            self.staircase_layers[staircase_start:]
+            + self.staircase_layers[:staircase_start]
+        )
+        x = x.new_zeros(0, bsz, edim)  # here x is equivalent to 'prev_layer_output'
+        for _nforwards, layer in enumerate(
+            islice(cycle(rolled_staircase), num_forwards)
+        ):
+            if chunk_queue:
+                x = torch.cat([x, chunk_queue.popleft()], dim=0)
+
+            if incremental_state is None and not full_context_alignment:
+                self_attn_mask = self.buffered_future_mask(x)
+            else:
+                self_attn_mask = None
+
+            curr_slen = x.shape[0]
+            curr_self_attn_padding_mask = (
+                self_attn_padding_mask[:, :curr_slen]
+                if self_attn_padding_mask is not None
+                else None
+            )
+
+            x, layer_attn, _ = layer(
+                x,
+                None,  # enc_out
+                None,  # encoder_padding_mask
+                incremental_state,
+                self_attn_mask=self_attn_mask,
+                self_attn_padding_mask=curr_self_attn_padding_mask,
+                need_attn=False,
+                need_head_weights=False,
+            )
+            inner_states.append(x)
+
+        for idx, layer in enumerate(self.top_layers):
+            if incremental_state is None and not full_context_alignment:
+                self_attn_mask = self.buffered_future_mask(x)
+            else:
+                self_attn_mask = None
+            x, layer_attn, _ = layer(
+                x,
+                None,  # enc_out
+                None,  # encoder_padding_mask
+                incremental_state,
+                self_attn_mask=self_attn_mask,
+                self_attn_padding_mask=self_attn_padding_mask,
+                need_attn=False,
+                need_head_weights=False,
+            )
+            inner_states.append(x)
+
+        if attn is not None:
+            # average probabilities over heads
+            attn = attn.mean(dim=0)  # type:ignore
+
+        if self.layer_norm is not None:
+            x = self.layer_norm(x)
+
+        # T x B x C -> B x T x C
+        x = x.transpose(0, 1)
+
+        if self.project_out_dim is not None:
+            x = self.project_out_dim(x)
+
+        return x, {"attn": [attn], "inner_states": inner_states}
 
 
 class TransformerAlibiDecoderLayerBase(TransformerDecoderLayerBase):
